a367194bcb607fadf12b76f0ac41c7d4e6a84563
SalmaNafisa <145917431+SalmaNafisa@users.noreply.github.com>
2023-10-30 13:27:34 +0700

    Add files via upload

---
+++ Kelipatan/src/kelipatan/Kelipatan.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kelipatan;
+
+/**
+ *
+ * @author ASUS TUF
+ */
+public class Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Inisialisasi variabel
+        float angka = 3.5f;
+
+        // Perulangan
+        while (angka <= 35) {
+            System.out.println(angka);
+            angka += 3.5f;
+        }
+    }
+    
+}
